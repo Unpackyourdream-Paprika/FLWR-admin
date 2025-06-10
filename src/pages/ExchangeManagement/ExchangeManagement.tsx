a89ba42a1a24c7acb2d423c4de1912e086fb8283
s2s2hyun <s2s2hyun0601@gmail.com>
2025-06-10 18:50:41 +0900

    [Feat] router add path

---
+++ src/pages/ExchangeManagement/ExchangeManagement.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ExchangeManagement() {
+  return (
+    <div>ExchangeManagement</div>
+  )
+}
---
+++ src/pages/PurchaseConfirmation/PurchaseConfirmation.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PurchaseConfirmation() {
+  return (
+    <div>PurchaseConfirmation</div>
+  )
+}
